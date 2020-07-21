176050b18dab6beb4a43f248d9035a14a1932f52
Isobella Johnson (1000094893) <JOHNIL1@student.op.ac.nz>
2020-07-22 10:13:59 +1200

    Add project files.

---
+++ CaculatorLab/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CaculatorLab
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            double num1 = Convert.ToInt16(textBox1.Text);
+            double num2 = Convert.ToInt16(textBox2.Text);
+            double divide = num1 / num2;
+            textBox3.Text = divide.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int num1 = Convert.ToInt16(textBox1.Text);
+            int num2 = Convert.ToInt16(textBox2.Text);
+            int multiply = num1 * num2;
+            textBox3.Text = multiply.ToString();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int num1 = Convert.ToInt16(textBox1.Text);
+            int num2 = Convert.ToInt16(textBox2.Text);
+            int subtract = num1 - num2;
+            textBox3.Text = subtract.ToString();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int num1 = Convert.ToInt16(textBox1.Text);
+            int num2 = Convert.ToInt16(textBox2.Text);
+            int addition = num1 + num2;
+            textBox3.Text = addition.ToString();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+        }
+    }
+}
